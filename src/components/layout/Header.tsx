bdcd3cb5577b9f863a62ff35a6bb48c8728e4a36
Manshigroupindia <manshigroupindia@gmail.com>
2026-08-29 14:47:28 +0530

    App Install Guide 8

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ import { Button } from '../ui/Button';
 
 export const Header: React.FC = () => {
   const navigate = useNavigate();
-  const { staffProfile } = useAuth();
+  const { userDoc, staffProfile } = useAuth();
   const { lockPinSession } = useSecurity();
   const { isStandalone, triggerInstall } = usePWA();
   const [timeStr, setTimeStr] = useState(getCurrentTimeFormatted());
